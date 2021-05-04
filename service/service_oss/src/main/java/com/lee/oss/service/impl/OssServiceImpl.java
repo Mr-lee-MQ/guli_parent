340c22a0339ca2ceb2119f25bee2b2582eda42a0
Mr.leeMQ <2285051311@qq.com>
2021-05-04 16:11:17 +0800

    默认的

--- service/service_oss/src/main/java/com/lee/oss/service/impl/OssServiceImpl.java
+++ service/service_oss/src/main/java/com/lee/oss/service/impl/OssServiceImpl.java
@@ -1,15 +1,57 @@
 package com.lee.oss.service.impl;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.lee.oss.service.OssService;
+import com.lee.oss.utils.ConstantPropertiesUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class OssServiceImpl implements OssService {
 
-
+    //上传头像到Oss
     @Override
     public String uploadFileAvatar(MultipartFile file) {
+        //通过工具类获取endpoint，accessKeyId，accessKeySecret，bucketName几个参数
+        String endpoint = ConstantPropertiesUtils.END_POINT;
+        String accessKeyId = ConstantPropertiesUtils.ACCESS_KEY_ID;
+        String accessKeySecret = ConstantPropertiesUtils.ACCESS_KEY_SECRET;
+        String bucketName = ConstantPropertiesUtils.BUCKET_NAME;
+
+
+        try {
+
+            // 创建OSSClient实例。
+            OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+
+            //获取文件输入流
+            InputStream inputStream = file.getInputStream();
+
+            //获取文件名称
+            String fileName = file.getOriginalFilename();
+            //调用oss方法实现上传
+            //第一个参数  bucket名称
+            //第二个参数  上传到oss文件路径和文件名称
+            //第三个参数  上传文件输入流
+
+            ossClient.putObject(bucketName, fileName, inputStream);
+
+            // 关闭OSSClient。
+            ossClient.shutdown();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
         return null;
     }
 }
