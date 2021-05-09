3869d5fc31a4578ceaf951884e23e15140a6c9ce
Mr.leeMQ <2285051311@qq.com>
2021-05-09 12:29:16 +0800

    默认的

--- common/service_base/src/main/java/com/lee/service_base/config/SwaggerConfig.java
+++ common/service_base/src/main/java/com/lee/service_base/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ package com.lee.service_base.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
