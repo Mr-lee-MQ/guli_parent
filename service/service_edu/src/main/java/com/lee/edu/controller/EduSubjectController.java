a2e5743c33417fc8031b4acfd3568060fa2f1866
Mr.leeMQ <2285051311@qq.com>
2021-05-19 11:21:49 +0800

    默认的

--- service/service_edu/src/main/java/com/lee/edu/controller/EduSubjectController.java
+++ service/service_edu/src/main/java/com/lee/edu/controller/EduSubjectController.java
@@ -5,6 +5,7 @@ import com.lee.common_utils.R;
 import com.lee.edu.service.EduSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +28,9 @@ public class EduSubjectController {
 
     //添加课程分类
     //通过上传过来的文件，把文件内容读取出来
+    @PostMapping("/addSubject")
     public R addSubject(){
-
+    //
 
 
         return R.ok();
