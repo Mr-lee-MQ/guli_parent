e9cad4772b96907b692eb664821f8d0bd96d76c5
Mr.leeMQ <2285051311@qq.com>
2021-05-19 13:24:03 +0800

    默认的

--- service/service_edu/src/main/java/com/lee/edu/listener/SubjectExcelListener.java
+++ service/service_edu/src/main/java/com/lee/edu/listener/SubjectExcelListener.java
@@ -2,6 +2,8 @@ package com.lee.edu.listener;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lee.edu.entity.EduSubject;
 import com.lee.edu.entity.excle.SubjectDate;
 import com.lee.edu.service.EduSubjectService;
 import com.lee.service_base.exceptionhandler.GuLiException;
@@ -26,9 +28,34 @@ public class SubjectExcelListener extends AnalysisEventListener<SubjectDate> {
         if (subjectDate==null){
             throw new GuLiException(20001,"Excel文件数据为空");
         }
+        //一行一行读取，每次读取有两个值，第一个值为一级分类，第二个值为二级分类
+        //判断一级分类是否重复
+        EduSubject oneSubject = this.existOneSubject(subjectService, subjectDate.getOneSubjectName());
+        if (oneSubject==null){//没有相同一级分类，进行添加
+
+        }
+
 
     }
 
+    //判断一级分类不能重复添加
+    private EduSubject existOneSubject(EduSubjectService subjectService,String name){
+        QueryWrapper<EduSubject> wrapper = new QueryWrapper<>();
+        wrapper.eq("title",name);
+        wrapper.eq("parent_id","0");
+        return subjectService.getOne(wrapper);
+
+    }
+
+
+    //判断二级分类不能重复添加
+    private EduSubject existTwoSubject(EduSubjectService subjectService,String name,String pid){
+        QueryWrapper<EduSubject> wrapper = new QueryWrapper<>();
+        wrapper.eq("title",name);
+        wrapper.eq("parent_id",pid);
+        return subjectService.getOne(wrapper);
+
+    }
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
 
